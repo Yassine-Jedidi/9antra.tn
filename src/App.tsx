3839e59f4334317557a23c2c928d33b478c83ea2
Yassine <yassinejedidi17@gmail.com>
2024-12-28 12:13:19 +0100

    initialize project (vite, react, tailwindcss)

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <div className="rounded-lg bg-white p-8 shadow-lg">
+        <h1 className="text-2xl font-bold text-gray-800">Hello Tailwind!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
